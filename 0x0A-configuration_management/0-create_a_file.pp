19f5b7c0f2ac1a909823894b8d57017e72b73f66
Natnael-Gedlu <nategd800@gmail.com>
2023-11-24 14:37:25 +0300

    Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # creates a file in /tmp directory.
 
 file { '/tmp/school':
-  ensure     => 'file',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
+
