86289b13b200d1c408963c995179572a13d7b2bf
Natnael-Gedlu <nategd800@gmail.com>
2023-11-28 22:47:17 +0300

    new corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,21 @@
-# Install Nginx package
+# script installs Nginx using puppet
+
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
-http {
-  server {
-    listen 80;
-    location / {
-      root   /var/www/html;
-      index  index.html;
-    }
-
-    location /redirect_me {
-      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-  }
-}
-events {}',
-  require => Package['nginx'],
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => file,
   content => 'Hello World!',
-  require => Package['nginx'],
 }
 
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => [Package['nginx'], File['/etc/nginx/sites-available/default']],
+  ensure  => running,
+  require => Package['nginx'],
 }
