d33a8fc26167ed685822f53e67106dbacb7f5ed1
Natnael-Gedlu <nategd800@gmail.com>
2024-01-25 15:45:42 +0300

    web stack debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Increases the hard and soft file limits for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
