d663a0a36f9c2609be121287da9ddf337075a272
Natnael-Gedlu <nategd800@gmail.com>
2024-01-29 16:19:47 +0300

    Web Stack Debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest replacing occurrences of the string "phpp" with "php" in the wp-settings.php file.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
